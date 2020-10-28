42033b3d58450a96532bd3a72414826bbc517e9b
Albert <thealbertyang@gmail.com>
2020-10-27 17:34:12 -0700

    - Create Button - Install Styled Components - Modify Button Space - Add Frequency Of Unique Chars Button to Home Page - Add Duplicate People Button to Home Page

---
+++ components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    padding: 0 20px;
+`;
+
+export default Button;
